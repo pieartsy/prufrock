8e9858ad786d2416b58b17bb702f08169c5fb4d8
pieartsy <loviballet@yahoo.com>
2023-02-16 18:07:11 -0500

    minimum viable product branch

--- prufrock/meta/script.js
+++ prufrock/meta/script.js
@@ -1,11 +1,13 @@
-//UIBar.destroy()
+UIBar.destroy()
+
+Config.addVisitedLinkClass = true;
 
 /* Delayed Text code - Start - by HiEv*/
 $(document).on(':passagerender', function (ev) {
 	// Find all elements containing the delayed class.
 	var elems = $(ev.content).find('.delayed');
 	// Appearance delay (in milliseconds) between each delayed text block.
-	var delay = 2000; // 1 second fade-in
+	var delay = 2000; // 2 second fade-in
 	if (elems.length > 0) {
 		elems.each(function (i) {
 			$(this)
@@ -15,3 +17,20 @@ $(document).on(':passagerender', function (ev) {
 	}
 });
 /* Delayed Text code - End */
+
+/* Boldtext code */
+$(document).on(':passagerender', function (ev) {
+	// Find all elements containing the endwords class.
+	var elems = $(ev.content).find('.endwords');
+	// Appearance delay (in milliseconds) between each delayed text block.
+	var delay = 500; // half second change
+	if (elems.length > 0) {
+		elems.each(function (i) {
+			$(this)
+				.delay(delay * (i + 1))
+				.animate({
+					fontWeight: '700'
+				});
+		});
+	}
+});
