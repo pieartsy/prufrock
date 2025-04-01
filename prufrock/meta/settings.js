a531458a425c0cc6db19829e1365185c8e5a71bb
Aster Fialla <asterfialla@gmail.com>
2025-04-01 10:25:37 -0400

    fixing font stuff and removing unused dialog functionality

--- prufrock/meta/settings.js
+++ prufrock/meta/settings.js
@@ -18,28 +18,3 @@
 //     onInit   : settingThemeHandler,
 //     onChange : settingThemeHandler
 // });
-
-/*=================TIME=================*/
-var settingDelayLengthNames = ["not enough (1 min - default)", "almost enough (3 min)", "barely enough (5 min)"]
-var settingDelayLengthHandler = function () {
-
-	// switch on the theme name to add the requested theme class
-	switch (settings.speed) {
-    case "not enough (1 min - default)":
-         State.variables.speed = 60;
-	case "almost enough (3 min)":
-		State.variables.speed = 180;
-		break;
-	case "barely enough (5 min)":
-  		State.variables.speed = 300;
-		break;
-	}
-};
-
-Setting.addList("timer", {
-    label    : "How much time do I have?",
-    default  : "not enough (1 min - default)",
-	list     : settingDelayLengthNames,
-	onInit   : settingDelayLengthHandler,
-	onChange : settingDelayLengthHandler
-});
